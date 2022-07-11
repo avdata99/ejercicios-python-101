8c430f7801f8a54a4d4c44be497487f0a0da73da
guosma <alekenemark@gmail.com>
2022-07-11 17:46:20 -0300

    resolucion ejercicio

--- ejercicios/ejercicio-010/ejercicio.py
+++ ejercicios/ejercicio-010/ejercicio.py
@@ -4,13 +4,15 @@ letra de la "palabra" (string) que se pasa como parametro.
 """
 
 
-def tercera_letra(palabra):
-    pass
+def tercera_letra(palabra:str) -> str:
+    return palabra[2]
 
 
 # NO BORRAR LAS LINEAS QUE SIGUEN
 # Una vez terminada la funcion ejecutar este archivo
 # Si se ve la leyenda 'Ejercicio terminado OK' esta listo, crear un PR
+
+print(tercera_letra("hola"))
 
 assert tercera_letra("hola") == "l"
 assert tercera_letra("Es facil") == " "
